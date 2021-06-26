d6bdff105138957dd22b308e462ad8c0f5397867
TomaszPielecki <74858305+TomaszPielecki@users.noreply.github.com>
2021-06-26 18:32:04 +0200

    zadanie3

--- src/pl/gda/wsbZaliczenie/Vehicle.java
+++ src/pl/gda/wsbZaliczenie/Vehicle.java
@@ -1,5 +1,25 @@
 package pl.gda.wsbZaliczenie;
 
-public class Vehicle {
+public abstract class Vehicle {
+    protected String registrationNumber;
+    protected String vinNumber;
+    protected String color;
+    protected double price;
+    protected double fuelConsumption;
+    protected double tankCondition;
+    protected double tankCapacity;
+    protected double odometer;
 
+
+    public Vehicle(String registrationNumber, String vinNumber, String color, double price,
+                   double fuelConsumption, double tankCondition, double odometer, double tankCapacity) {
+        this.registrationNumber = registrationNumber;
+        this.vinNumber = vinNumber;
+        this.color = color;
+        this.price = price;
+        this.fuelConsumption = fuelConsumption;
+        this.tankCondition = tankCondition;
+        this.odometer = odometer;
+        this.tankCapacity = tankCapacity;
+    }
 }
