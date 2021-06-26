4b995d82b165105bd20077fd6ba43662b2d38296
TomaszPielecki <74858305+TomaszPielecki@users.noreply.github.com>
2021-06-26 18:45:29 +0200

    zadanie5

--- src/pl/gda/wsbZaliczenie/Car.java
+++ src/pl/gda/wsbZaliczenie/Car.java
@@ -1,8 +1,9 @@
 package pl.gda.wsbZaliczenie;
 
 public class Car extends Vehicle{
-
-    public Car(String registrationNumber, String vinNumber, String color, double price, double fuelConsumption, double tankCondition, double odometer, double tankCapacity) {
+    protected int numberOfDoors;
+    public Car(String registrationNumber, String vinNumber, String color, double price, double fuelConsumption, double tankCondition, double odometer, double tankCapacity, int numberOfDoors) {
         super(registrationNumber, vinNumber, color, price, fuelConsumption, tankCondition, odometer,tankCapacity);
+        this.numberOfDoors = numberOfDoors;
     }
 }
--- src/pl/gda/wsbZaliczenie/Truck.java
+++ src/pl/gda/wsbZaliczenie/Truck.java
@@ -1,9 +1,10 @@
 package pl.gda.wsbZaliczenie;
 
 public class Truck extends Vehicle{
-
-    public Truck(String registrationNumber, String vinNumber, String color, double price, double fuelConsumption, double tankCondition, double odometer, double tankCapacity) {
+    protected double capacity;
+    public Truck(String registrationNumber, String vinNumber, String color, double price, double fuelConsumption, double tankCondition, double odometer, double tankCapacity,double capacity) {
         super(registrationNumber, vinNumber, color, price, fuelConsumption, tankCondition, odometer,tankCapacity);
+        this.capacity = capacity;
 
     }
 }
